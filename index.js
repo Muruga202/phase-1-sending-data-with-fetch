40f1225c651efc2da6d73a86e00c4f6b5395f58f
Maxwell Muruge <maxwell.muruga@student.moringaschool.com>
2025-03-28 01:21:23 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      // Append the new id to the DOM
+      document.body.innerHTML += `<p>${object.id}</p>`;
+    })
+    .catch(function (error) {
+      // Append the error message to the DOM
+      document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+}
